f82b7ff0c276bd06215c7eff0913a240dfcf2ada
Yaroslavdi <76851160+Yaroslavdi@users.noreply.github.com>
2022-06-02 18:36:53 +0300

    Commit1

---
+++ Ch/src/main/java/com/example/chemistry/Chemistry.java
@@ -0,0 +1,11 @@
+package com.example.chemistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chemistry {
+	public static void main(String[] args) {
+		SpringApplication.run(Chemistry.class, args);
+	}
+}
---
+++ Ch/src/test/java/com/example/chemistry/ChemistryTests.java
@@ -0,0 +1,13 @@
+package com.example.chemistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
